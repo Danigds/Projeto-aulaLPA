6877aad8b832b7498aec1cc0b0e6745519085812
Danigds <danigds86@gmail.com>
2024-04-19 15:34:23 -0300

    Signed-off-by: Danigds <danigds86@gmail.com>

---
+++ Diagramaclassejogo/Backgroud.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Backgroud import Backgroud
+from Backgroud import Backgroud
+from Backgroud import Backgroud
+from Entity import Entity
+
+class Backgroud(Backgroud, Backgroud, Backgroud, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
---
+++ Diagramaclassejogo/Menu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.Surface = None
+        self.Attribute1 = None
+        self.window Surface = None
+        self.Attribute2 = None
+
+    def run(self, ):
+        pass
+
